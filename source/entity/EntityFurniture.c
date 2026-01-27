59178c014ecfae1ee0d80c524f8aaf1ee0e65615
KaliLugu <besnieradr@bbox.fr>
2026-01-27 18:35:35 +0000

    entityFurniture.c

--- source/entity/EntityFurniture.c
+++ source/entity/EntityFurniture.c
@@ -4,6 +4,28 @@
 #include "../Item.h"
 #include "../Render.h"
 
+// to mose, see comment in main.c
+int itemGetLegacyId(ItemID id)
+{
+    switch (id.category)
+    {
+        case ITEM_CATEGORY_TOOL:
+            if (id.id >= toolItemCount) return 0;
+            return toolItems[id.id].legacy_id;
+
+        case ITEM_CATEGORY_FOOD:
+            if (id.id >= foodItemCount) return 0;
+            return foodItems[id.id].legacy_id;
+
+        case ITEM_CATEGORY_GENERIC:
+            if (id.id >= genericItemCount) return 0;
+            return genericItems[id.id].legacy_id;
+
+        default:
+            return 0;
+    }
+}
+
 void assignInventory(Entity *e) {
     if (eManager.nextInv > 300)
         return;
@@ -21,9 +43,9 @@ Entity newEntityFurniture(int itemID, Inventory *invPtr, int x, int y, uByte lev
     e.yr = 3;
     e.entityFurniture.itemID = itemID;
     e.canPass = false;
-    if (itemID == ITEM_LANTERN) {
+    if (itemID == itemGetLegacyId((ItemID){ITEM_CATEGORY_FURNITURE, 5})) { // Lantern
         e.entityFurniture.r = 8;
-    } else if (itemID == ITEM_CHEST) { // TODO: This should not happen here, the new functions should not mess with any outside state
+    } else if (itemID == itemGetLegacyId((ItemID){ITEM_CATEGORY_FURNITURE, 1})) { // TODO: This should not happen here, the new functions should not mess with any outside state
         if (invPtr == NULL)
             assignInventory(&e);
         else
