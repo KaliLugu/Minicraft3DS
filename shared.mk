61ce095d221a61650d33196615fedda6c945af65
Adrien Besnier <besnieradr@bbox.fr>
2026-03-26 11:31:47 +0100

    delete-switch-support (#7)

--- shared.mk
+++
@@ -1,7 +0,0 @@
-SOURCES		:=	source source/minizip source/data source/menu source/editor source/network source/ingamemenu source/entity source/render source/engine
-INCLUDES	:=	include
-ROMFS		:=	resources
-
-APP_TITLE	:= Minicraft
-APP_DESCRIPTION	:= Minicraft was originally created by Markus "Notch" Perrson.
-APP_AUTHOR	:= Davideesk/andre111/adrien
