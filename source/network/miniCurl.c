857232ab74224f0276cd96c8438cad2dea882bb8
KaliLugu <besnieradr@bbox.fr>
2026-04-25 15:06:03 +0000

    add internetInit() & change readme

--- source/network/miniCurl.c
+++ source/network/miniCurl.c
@@ -5,6 +5,15 @@ typedef struct {
     size_t size;
 } Response;
 
+int internetInit() {
+    if (R_FAILED(ret)) {
+        fprintf(stderr, "httpcInit failed: 0x%08lX\n", ret);
+        return -1;
+    }
+    socInit((u32*)memalign(0x1000, 0x100000), 0x100000); // socket memory
+    return 0;
+}
+
 size_t write_chunk(void *data, size_t size, size_t nmemb, void *userdata);
 
 char *miniCurlGet(const char *url) {
