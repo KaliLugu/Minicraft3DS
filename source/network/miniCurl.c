2016d30b5d8272a754daf56798ebbd037b71562c
KaliLugu <besnieradr@bbox.fr>
2026-04-25 15:29:28 +0000

    miniCurl & change readme

--- source/network/miniCurl.c
+++ source/network/miniCurl.c
@@ -1,11 +1,17 @@
 #include "miniCurl.h"
 
+static u32 *_socBuffer = NULL;
+
 typedef struct {
     char *string;
     size_t size;
 } Response;
 
+
+size_t write_chunk(void *data, size_t size, size_t nmemb, void *userdata);
+
 int internetInit() {
+    Result ret = httpcInit(0);
     if (R_FAILED(ret)) {
         fprintf(stderr, "httpcInit failed: 0x%08lX\n", ret);
         return -1;
@@ -14,11 +20,47 @@ int internetInit() {
     return 0;
 }
 
-size_t write_chunk(void *data, size_t size, size_t nmemb, void *userdata);
+int exitInternet() {
+    if (_socBuffer) {
+        free(_socBuffer);
+        _socBuffer = NULL;
+    }
+    return 0;
+}
+
+
 
 char *miniCurlGet(const char *url) {
-    // need to verify access to romfs
-    // check certificate validation
-    // process url
-    // return response string
+    Response response;
+    response.string = malloc(1);
+    response.size = 0;
+
+    CURL *curl;
+    CURLcode result;
+
+    curl = curl_easy_init();
+
+    if (!curl) {
+        fprintf(stderr, "Failed to initialize curl\n");
+        return NULL;
+    }
+    if (curl == NULL) {
+        fprintf(stderr, "curl_easy_init() failed\n");
+        return NULL;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_CAINFO, "romfs:/cacert.pem");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_chunk);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&response);
+
+    result = curl_easy_perform(curl);
+    if (result != CURLE_OK) {
+        fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));
+        curl_easy_cleanup(curl);
+        free(response.string);
+        return NULL;
+    }
+    return response.string;
 }
--- source/network/miniCurl.h
+++ source/network/miniCurl.h
@@ -1,8 +1,11 @@
-#pragma once
-#include <stdio.h>
 #include <curl/curl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
+#include <stddef.h>
+#include <3ds.h>
 
+extern int internetInit();
+extern int exitInternet();
 extern char *miniCurlGet(const char *url);
