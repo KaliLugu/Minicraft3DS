3202d79dff4eee644b32ec34aa086b351e798019
Adrien Besnier <besnieradr@bbox.fr>
2026-08-05 03:14:09 +0000

    Revert "No issue/store last started version infile (#19)" (#20)
    This reverts commit 4d3751887066a4eaf0c200f0c3542d2603a58b55.

--- source/menu/MenuTitle.c
+++ source/menu/MenuTitle.c
@@ -97,10 +97,6 @@ void menuTitleRender(int screen, int width, int height) {
             renderText("new update is available", 0, 112);
         }
 
-        if (showChangeLog) {
-            // need to create popup for this
-        }
-
         renderTitle(76, 12);
 
         for (int i = 5; i >= 0; --i) {
